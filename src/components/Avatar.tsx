fa654718c4f5541210e63c2ec6044fa1498c6dea
Igor Pletnjov <ipletnjov@twilio.com>
2023-07-20 23:41:33 +0300

    feat: Add tooltips

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,6 +1,7 @@
 import { Avatar as PasteAvatar } from "@twilio-paste/avatar";
 import { UserIcon } from "@twilio-paste/icons/cjs/UserIcon";
 import { IconSize } from "@twilio-paste/style-props";
+import { Tooltip } from "@twilio-paste/core";
 
 type AvatarProps = {
   name: string;
@@ -9,11 +10,28 @@ type AvatarProps = {
 
 // name = friendlyName ?? identity
 const Avatar: React.FC<AvatarProps> = ({ name, size }) => {
-	if (name.startsWith("whatsapp:") || name.startsWith("sms:") || name.startsWith("+")) {
-		return <PasteAvatar size={size ?? "sizeIcon70"} variant="user" name={name} icon={UserIcon} />
-	}
-	return <PasteAvatar size={size ?? "sizeIcon70"} variant="user" name={name} />
-	// use src for specified avatar images - once we have them!
+  if (
+    name.startsWith("whatsapp:") ||
+    name.startsWith("sms:") ||
+    name.startsWith("+")
+  ) {
+    return (
+      <Tooltip text={name} placement={"bottom-start"}>
+        <PasteAvatar
+          size={size ?? "sizeIcon70"}
+          variant="user"
+          name={name}
+          icon={UserIcon}
+        />
+      </Tooltip>
+    );
+  }
+  return (
+    <Tooltip text={name} placement={"bottom-start"}>
+      <PasteAvatar size={size ?? "sizeIcon70"} variant="user" name={name} />
+    </Tooltip>
+  );
+  // use src for specified avatar images - once we have them!
 };
 
 export { Avatar };
