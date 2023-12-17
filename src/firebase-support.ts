b57be6ad985d0bde014f0992ccb646a99a0b9b03
anp011494 <150107362+anp011494@users.noreply.github.com>
2023-12-17 15:10:21 -0800

    changes config

--- src/firebase-support.ts
+++ src/firebase-support.ts
@@ -11,9 +11,18 @@ let app: FirebaseApp;
 let messaging: Messaging;
 let initialized = false;
 
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
 try {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  app = initializeApp((window as any).firebaseConfig);
+  app = initializeApp(firebaseConfig);
   messaging = getMessaging(app);
   initialized = true;
 } catch {
