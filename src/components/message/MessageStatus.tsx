326c60e7f51ffc96e50798d7eff3979583a65b1e
Can Citoglu <ccitoglu@twilio.com>
2025-09-15 13:32:57 +0300

    fix: Fix message status icons

--- src/components/message/MessageStatus.tsx
+++ src/components/message/MessageStatus.tsx
@@ -5,10 +5,11 @@ import {
 } from "../../store/reducers/messageListReducer";
 import { ReduxParticipant } from "../../store/reducers/participantsReducer";
 import { useEffect, useState } from "react";
-import DeliveredIcon from "../icons/Delivered";
 import SendingIcon from "../icons/Sending";
 import FailedIcon from "../icons/Failed";
 import ReadIcon from "../icons/Read";
+import Sent from "../icons/Sent";
+import Delivered from "../icons/Delivered";
 
 type MessageStatusProps = {
   message: ReduxMessage;
@@ -35,41 +36,31 @@ const MessageStatus: React.FC<MessageStatusProps> = (props) => {
 
   return (
     <>
-      {status === MessageStatusType.Delivered ? (
-        <>
-          <DeliveredIcon
-            style={{ ...statusStyle, ...statusIconStyle }}
-            color="green"
-          />
-        </>
-      ) : null}
-      {status === MessageStatusType.Sent ? (
-        <>
-          <SendingIcon style={{ ...statusStyle, ...statusIconStyle }} />
-        </>
-      ) : null}
-      {status == MessageStatusType.None ? (
-        <>
-          <SendingIcon style={{ ...statusStyle, ...statusIconStyle }} />
-        </>
+      {status === MessageStatusType.Sending ||
+      status == MessageStatusType.None ? (
+        <SendingIcon style={{ ...statusStyle, ...statusIconStyle }} />
       ) : null}
 
       {status === MessageStatusType.Failed ? (
-        <>
-          <FailedIcon
-            style={{ ...statusStyle, ...statusIconStyle }}
-            color="red"
-          />
-        </>
+        <FailedIcon
+          style={{ ...statusStyle, ...statusIconStyle }}
+          color="red"
+        />
+      ) : null}
+
+      {status === MessageStatusType.Sent ? (
+        <Sent style={{ ...statusStyle, ...statusIconStyle }} />
+      ) : null}
+
+      {status === MessageStatusType.Delivered ? (
+        <Delivered style={{ ...statusStyle, ...statusIconStyle }} />
       ) : null}
 
       {status === MessageStatusType.Read ? (
-        <>
-          <ReadIcon
-            style={{ ...statusStyle, ...statusIconStyle }}
-            color="green"
-          />
-        </>
+        <ReadIcon
+          style={{ ...statusStyle, ...statusIconStyle }}
+          color="green"
+        />
       ) : null}
     </>
   );
